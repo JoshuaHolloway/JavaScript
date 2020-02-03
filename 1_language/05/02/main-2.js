08877620150cd2e1d6fe73c5e86046b5c523f8e2
JoshuaHolloway <jhollow6@asu.edu>
2020-02-03 17:40:03 -0600

    JS-Engine

--- 1_language/05/02/main-2.js
+++ 1_language/05/02/main-2.js
@@ -1,6 +1,10 @@
 const addListenerBtn = document.getElementById('add-listener-btn');
-const clickableBtn = document.getElementById('clickable-ben');
+const clickableBtn = document.getElementById('clickable-btn');
 const messageInput = document.getElementById('click-message-input');
+
+let person = { name: 'josh' };
+person = null;
+// The address is no-longer stored in this variable
 
 function printMessage() {
     const value = messageInput.value;
