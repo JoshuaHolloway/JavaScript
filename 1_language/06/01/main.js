9571d9c0b4648c19f41e01d876cb4dac07be6c0d
JoshuaHolloway <jhollow6@asu.edu>
2020-02-04 16:45:15 -0600

    Functions are Objects!

---
+++ 1_language/06/01/main.js
@@ -0,0 +1,44 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+    console.log('Game is starting');
+}
+
+// Can invoke synchronously:
+startGame();
+
+// Can use asynchronously via callback on event-listener:
+startGameBtn.addEventListener('click', startGame);
+
+// NOTE: We are calling addEventListener like a method off the startGameBtn Object
+
+// // RECALL: Objects can have properties and methods
+// const person = {
+
+//     // Property
+//     name: 'josh',
+
+//     // Method
+//     method_1: () => {
+//         console.log(this.name + ' from method_1!');
+
+//         // why does this.name not work here?
+//     }
+// };
+// console.log(person.name);
+// person.method_1();
+
+
+console.log('typeof startGame: ');
+console.log(typeof startGame);
+
+console.log('console.log(startGame): ');
+console.log(startGame);
+
+// NOTE the key-value pairs in the function:
+console.log('console.dir(startGame):');
+console.dir(startGame);
+
+// => Function are Objects:
+// -They are therefore stored on the heap
+// -They are Reference types
