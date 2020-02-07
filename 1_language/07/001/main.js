f76c93f2d8511e1642cec0a32b9d08391803036d
JoshuaHolloway <jhollow6@asu.edu>
2020-02-06 18:10:57 -0600

    Selecting elements in the DOM

---
+++ 1_language/07/001/main.js
@@ -0,0 +1,8 @@
+const x = document.getElementById('main-title');
+console.log(x);
+console.dir(x);
+
+const list_items = document.getElementsByClassName('list-item');
+console.log(list_items);
+for (let list_item of list_items)
+    console.log(list_item);
