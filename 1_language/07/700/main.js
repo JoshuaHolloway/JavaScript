cc2ab36562ca86cbac1090525402d6c18b64a2d4
JoshuaHolloway <jhollow6@asu.edu>
2020-02-07 14:20:32 -0600

    Document and Window Object

---
+++ 1_language/07/700/main.js
@@ -0,0 +1,12 @@
+console.log(document);
+console.dir(document);
+
+
+// -window is the real top-most object
+// -window gives you access to all the 
+//  core APIs that the browser wants to expose to you
+// -window represents a tab in the browser
+console.log(window);
+
+// -document object is a property of window
+// -document.xxx is really window.document.xxx
