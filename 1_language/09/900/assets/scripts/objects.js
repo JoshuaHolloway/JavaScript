bf9c1e6a6f2b4be5dcfb81e15903ca673e8b78e7
JoshuaHolloway <jhollow6@asu.edu>
2020-02-12 22:33:10 -0600

    Objects recap

---
+++ 1_language/09/900/assets/scripts/objects.js
@@ -0,0 +1,16 @@
+const person = {
+    name: 'josh',
+    greet: function () {
+        alert('hi from method');
+    }
+};
+
+// Create object
+console.log(person.name);
+person.greet();
+
+
+// Log:
+// -1 object
+// -1 arrays
+// -Legs VERY slightly sore and core not sore at all
