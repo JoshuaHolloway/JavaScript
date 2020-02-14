4746601d99d4b780fec4cda5009d4100510151a2
JoshuaHolloway <jhollow6@asu.edu>
2020-02-13 21:59:39 -0600

    splice()

--- 1_language/08/803/app.js
+++ 1_language/08/803/app.js
@@ -13,6 +13,16 @@ console.log(x);
 
 // splice deletes the elems and then inserts the new elems
 z = [1, 2, 3, 4, 5];
-z.splice(1, 3, 'a');
+const removed = z.splice(1, 3, 'a', 'b');
+console.log(z);
+console.log(removed);
+
+// negative indices wrap around (start at 5 and delete one elem)
+z = [0, 1, 2, 3, 4, 5];
+z.splice(-1, 1);
 console.log(z);
 
+// start at 4 and delete 2-elems
+z = [0, 1, 2, 3, 4, 5];
+z.splice(-2, 2);
+console.log(z);
