1d93460b104e71967a98d999c2d89ece39d8f063
JoshuaHolloway <jhollow6@asu.edu>
2020-02-13 22:36:58 -0600

    Adding, modifying, and deleting properties

--- 1_language/09/901/assets/scripts/objects.js
+++ 1_language/09/901/assets/scripts/objects.js
@@ -1,16 +1,47 @@
-const person = {
+let person = {
     name: 'josh',
     greet: function () {
         alert('hi from method');
     }
 };
 
-// Create object
-console.log(person.name);
-person.greet();
+// ...
 
+person = {
+    name: 'josh',
+    greet: function () {
+        alert('hi from method');
+    },
+    added_property: true
+};
 
-// Log:
-// -1 object
-// -1 arrays
-// -Legs VERY slightly sore and core not sore at all
+// ...
+
+// Can add properties like this:
+person.another_added_prop = true;
+
+console.log(person.another_added_prop);
+
+// Delete a property like this:
+delete person.name;
+console.log('name: ' + person.name);
+
+// This is the same thing (but frowned upon):
+person.greet = undefined;
+
+// what is difference between null and undefined?
+const x = {
+    prop1: 'a'
+};
+delete x.prop1;
+console.dir(x);
+// - - - - 
+const y = {
+    prop2: 'b'
+};
+y.prop2 = null;
+console.dir(y);
+
+// -null means that the object has that property, it is just not defined yet.
+// -undefined is like: Hey dude, this property doesn't even live in this freaking object!!!
+
