e2d84a68437f7aed3561dec5f4ad4cac18f0eb12
JoshuaHolloway <jhollow6@asu.edu>
2020-02-13 22:23:08 -0600

    Traversing child nodes

---
+++ 1_language/07/707/main.js
@@ -0,0 +1,34 @@
+const ul = document.querySelector('ul');
+console.log(ul);
+console.dir(ul);
+
+// Grab all children
+const children = ul.children;
+console.dir(children);
+
+// Grab the 1st element
+const child = children[0];
+console.log(child);
+
+child_nodes = ul.childNodes;
+// White-space/line-break are treated as children?!?!?!
+// That's f'd up man
+console.log(child_nodes);
+// Aint nobody got time for that!
+
+// Get last element (css-way)
+const x = document.querySelector('li:last-of-type');
+console.log(x);
+
+// More stuff:
+const a = ul.firstChild;
+console.log(a);
+
+const b = ul.firstElementChild;
+console.log(b);
+
+const d = ul.lastChild;
+console.log(d);
+
+const c = ul.lastElementChild;
+console.log(c);
