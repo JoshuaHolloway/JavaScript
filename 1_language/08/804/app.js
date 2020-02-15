fe484e6458cbb6a0ae47ac4ab7710d9f6d31433b
JoshuaHolloway <jhollow6@asu.edu>
2020-02-14 23:05:33 -0600

    slice

--- 1_language/08/804/app.js
+++ 1_language/08/804/app.js
@@ -1,28 +1,15 @@
-x = ['a', 'b', 'c'];
+const x = [1, 2, 3, 4, 5, 6];
+const x2 = x; // Copy by referece
 
-// start index
-// number of items to delete
-x.splice(1, 0, 'z');
+// Creates new copy
+const y = x.slice(); // Copy by value
+
+// Modify x:
+x.push(7);
+
+// x2 is copy by reference so changing x changes x2 :(
 console.log(x);
+console.log(x2);
 
-x.splice(1, 0, 'g');
-console.log(x);
-
-x.splice(1, 3);
-console.log(x);
-
-// splice deletes the elems and then inserts the new elems
-z = [1, 2, 3, 4, 5];
-const removed = z.splice(1, 3, 'a', 'b');
-console.log(z);
-console.log(removed);
-
-// negative indices wrap around (start at 5 and delete one elem)
-z = [0, 1, 2, 3, 4, 5];
-z.splice(-1, 1);
-console.log(z);
-
-// start at 4 and delete 2-elems
-z = [0, 1, 2, 3, 4, 5];
-z.splice(-2, 2);
-console.log(z);
+// y is copied by value so changing x doesn't affect y :)
+console.log(y);
