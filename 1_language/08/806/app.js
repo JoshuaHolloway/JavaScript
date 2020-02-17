0694fd51bd815865ed5da7d02448da0963011c56
JoshuaHolloway <jhollow6@asu.edu>
2020-02-16 23:34:35 -0600

    Optional second arg

--- 1_language/08/806/app.js
+++ 1_language/08/806/app.js
@@ -2,5 +2,9 @@ const x = ['a', 'b', 'c', 'd'];
 
 
 // Return index of arguement
-const index = console.log(x.indexOf('a'));
+const index = x.indexOf('a');
 console.log(index);
+
+// Optional second arg to specify index to start at
+const index2 = x.indexOf('a', 2);
+console.log(index2); // not found
