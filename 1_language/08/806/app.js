45481f3cfc26cf767f0c7731e2fe90f5f721c0f1
JoshuaHolloway <jhollow6@asu.edu>
2020-02-16 23:32:44 -0600

    indexOf()

--- 1_language/08/806/app.js
+++ 1_language/08/806/app.js
@@ -1,19 +1,6 @@
-const x = [1, 2];
-const y = [3, 4];
+const x = ['a', 'b', 'c', 'd'];
 
-const z = x.concat(y);
 
-console.log(x);
-console.log(y);
-console.log(z);
-
-// A copy by VALUE has occured
-x.push(['a', 'b'])
-console.log(z);
-
-y.push(['i', 'j']);
-console.log(z);
-
-z.push(['s', 't'])
-console.log(x);
-console.log(y);
+// Return index of arguement
+const index = console.log(x.indexOf('a'));
+console.log(index);
