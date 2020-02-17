3b47f4a2816b0a3cf54e2344739426cf05ee52e5
JoshuaHolloway <jhollow6@asu.edu>
2020-02-16 23:38:53 -0600

    latIndexOf()

--- 1_language/08/806/app.js
+++ 1_language/08/806/app.js
@@ -8,3 +8,12 @@ console.log(index);
 // Optional second arg to specify index to start at
 const index2 = x.indexOf('a', 2);
 console.log(index2); // not found
+
+// Search stops at first found entry
+const y = ['a', 'b', 'c', 'd', 'a', 'e'];
+const index3 = y.indexOf('a');
+console.log(index3);
+
+// Start searching from right
+const index4 = y.lastIndexOf('a');
+console.log(index4);
