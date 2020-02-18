2500cf3f311077d99ecf5469acdcbfe1cfab167f
JoshuaHolloway <jhollow6@asu.edu>
2020-02-17 23:50:46 -0600

    Finding stuff: find() and findIndex()

--- 1_language/08/807/app.js
+++ 1_language/08/807/app.js
@@ -23,3 +23,20 @@ const personData = [{ name: 'Josh' }, { name: 'steve' }];
 const index5 = personData.indexOf({ name: 'Josh' });
 console.log(index5);
 // -1 indicates not found
+
+// [807]
+// -Objects are reference values
+// -
+const josh = personData.find((person, index, persons) => {
+    return person.name === 'Josh';
+});
+// -Anonymous function arguement can accept up to 3 args:
+//  --1. Single object of the array
+//  --2. Index of the single element
+//  --3. The whole array
+// -J.S. executes this anonymous function on every
+//  element in the array
+//      --The function returns true for the element we 
+//        are looking for and false for all other elements.
+//      --It will stop after finding the first instance.
+console.log(josh);
