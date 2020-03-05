f4e86b06e227f68a82f08352c3f2d1aca59feb34
JoshuaHolloway <jhollow6@asu.edu>
2020-03-04 23:13:28 -0600

    Prototypes

--- 1_language/11/1102/app.js
+++ 1_language/11/1102/app.js
@@ -34,3 +34,27 @@ person2.greet();
 
 // Doesn't work without new keyword
 // const person3 = Person2();
+
+// ================================
+
+// [1102]: Prototypes
+// -Every constructor function you build has a special prototype property
+// -Which is not added to the objects we create based on it
+//  because its not part of the function body, but instead 
+//  a property of that function object.
+// -Recall: Functions ARE objects!
+// -The prototype is there by default
+//  (we can also edit it).
+// -It is automatically assigned as a prototype
+//  to the object which is created when we instantiate that
+//  constructor function.
+// -A prototype is an object itself
+//  (and every object has such a prototype).
+// -Prototypes are how objects share code.
+// -You should think of prototypes as fallback objects.
+//      Prototype Objects == "Fallback Objects"
+
+const p = new Person();
+p.greet();
+console.log(p.toString());
+// .toString() is a method that is prototypically inheritted
